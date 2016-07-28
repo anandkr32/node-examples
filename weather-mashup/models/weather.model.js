f7ed209f452ace5b63d1824c5051079ed8e30e56
Keshavaprasad B S <kbsbng@gmail.com>
2016-07-28 10:37:14 +0530

    updates to api endpoints

--- weather-mashup/models/weather.model.js
+++ weather-mashup/models/weather.model.js
@@ -1,8 +1,9 @@
 "use strict";
 
-var async, request;
+var async, request, log;
 async = require("async");
 request = require("request");
+log = require("bunyan").createLogger({"name": "weatherapp:model:weather"});
 
 class Weather {
 
@@ -13,17 +14,17 @@ class Weather {
             yahoo: function getWeatherFromYahoo(cb) {
                 var url;
 
-
-                url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.placefinder%20where%20text%3D%22" + place +"%22%20limit%201)&format=json&callback=";
+                url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.places%20where%20text%3D%22" + place + "%22%20limit%201&format=json&callback=";
+                log.info("hitting yahoo API", {url: url});
                 request(url, function (err, resp, body) {
                     body = JSON.parse(body);
-                    cb(err, body.query.results.channel.item);
+                    cb(err, body.query.results);
                 });
             },
             openweathermap: function getWeatherFromOpenWeatherMap(cb) {
                 var url;
 
-                url = "http://api.openweathermap.org/data/2.5/weather?q=" + place + "&appid=2de143494c0b295cca9337e1e96b00e0";
+                url = "http://api.openweathermap.org/data/2.5/weather?q=" + place + "&appid=7680f2b1ed8ffb99d964a78db16b5263";
                 request(url, function (err, resp, body) {
                     body = JSON.parse(body);
                     cb(err, body);
